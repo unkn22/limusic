515d2e8a9e903ad47ed5e5e8dcda092b7835014f
UnknownTv <126357184+unkn22@users.noreply.github.com>
2024-10-20 19:33:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "MTAxMzM4Mzc0NzM4ODg0NjA5MQ.Gvx9ZO.g08UQMznztZfbEiFNki-Bd_mc6n0ZCQsstpzqA", //Discord Bot Token
+  Token: process.env.Token || "", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "1013383747388846091", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "K0Vc8xXSJaWHkwiGoZPIAGE8OTJwtpv9", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
